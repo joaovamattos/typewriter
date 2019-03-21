89b66189753d3e80a1913785d92014fc16192e49
Arthur Vinicius <arthurviniciusvha@gmail.com>
2019-03-20 20:57:46 -0400

    Carousel Slider

---
+++ assets/js/util.js
@@ -0,0 +1,53 @@
+//---typeWriter---
+function typeWrite(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i ) => {
+        setTimeout( () => { elemento.innerHTML += letra; }, 75 * i)
+    });
+}
+
+const titulo = document.querySelector('h1');
+typeWrite(titulo);
+
+//---carousel---
+const carouselSlide = document.querySelector('.carousel-slide')
+const carouselItems = document.querySelectorAll('.carousel-item')
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+//Counter
+let counter = 1
+const size = carouselItems[0].clientWidth
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+
+//Button Listener
+nextBtn.addEventListener('click', ()=>{
+    if (counter >= carouselItems.length -1) return;
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out'
+    counter++
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'   
+})
+
+prevBtn.addEventListener('click', ()=>{
+    if (counter <= 0) return;
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out'
+    counter--
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'   
+})
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    if (carouselItems[counter].id === 'lastClone'){
+        carouselSlide.style.transition = 'none'
+        counter = carouselItems.length - 2
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'   
+    }
+    if (carouselItems[counter].id === 'firstClone'){
+        carouselSlide.style.transition = 'none'
+        counter = carouselItems.length - counter
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'   
+    }
+})
